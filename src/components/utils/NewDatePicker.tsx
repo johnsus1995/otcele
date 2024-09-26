828f7f467df75499e0cfefedb54255a9547fcf27
Jaison John <jaison.john@ileafsolutions.com>
2024-09-26 10:57:52 +0530

    fix: datepicker typeerror

--- src/components/utils/NewDatePicker.tsx
+++ src/components/utils/NewDatePicker.tsx
@@ -10,58 +10,61 @@ interface NewDatePickerProps {
   maxDate?: Date;
   minDate?: Date;
   dateFormat?: string;
+  onChange?: (date: Date | null) => void;
+  value?: Date | null;
+  className?: string;
 }
 
-const NewDatePicker = React.forwardRef<
-  HTMLInputElement | NewDatePickerProps,
-  any
->((props, ref) => {
-  const {
-    className,
-    onChange,
-    value,
-    maxDate = new Date(),
-    minDate,
-    showIcon,
-    dateFormat = 'MM/dd/YYYY',
-    ...otherProps
-  } = props;
+const NewDatePicker = React.forwardRef<HTMLInputElement, NewDatePickerProps>(
+  (props, ref) => {
+    const {
+      className,
+      onChange,
+      value,
+      maxDate = new Date(),
+      minDate,
+      showIcon,
+      dateFormat = 'MM/dd/YYYY',
+      ...otherProps
+    } = props;
 
-  return (
-    <div className=''>
-      <DatePicker
-        maxDate={maxDate}
-        minDate={minDate}
-        className=''
-        showMonthDropdown
-        showYearDropdown
-        // dropdownMode='select'
-        showPopperArrow={false}
-        placeholderText='MM/DD/YYYY'
-        disabled={false}
-        popperClassName='z-50'
-        customInput={
-          <input
-            className={cn(
-              `flex h-10 w-full rounded-md border border-input 
-              bg-background px-3 py-2 text-sm ring-offset-background 
-              file:border-0 file:bg-transparent file:text-sm file:font-medium 
-              placeholder:text-muted-foreground focus-visible:outline-none 
-              focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 
-              disabled:cursor-not-allowed disabled:opacity-50`,
-              className,
-              { ' datePicker-icon-class': showIcon },
-            )}
-            ref={ref}
-            {...otherProps}
-          />
-        }
-        selected={value}
-        onChange={onChange}
-        dateFormat={dateFormat}
-      />
-    </div>
-  );
-});
+    return (
+      <div className=''>
+        <DatePicker
+          maxDate={maxDate}
+          minDate={minDate}
+          className=''
+          showMonthDropdown
+          showYearDropdown
+          showPopperArrow={false}
+          placeholderText='MM/DD/YYYY'
+          disabled={false}
+          popperClassName='z-50'
+          customInput={
+            <input
+              className={cn(
+                `flex h-10 w-full rounded-md border border-input 
+                bg-background px-3 py-2 text-sm ring-offset-background 
+                file:border-0 file:bg-transparent file:text-sm file:font-medium 
+                placeholder:text-muted-foreground focus-visible:outline-none 
+                focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 
+                disabled:cursor-not-allowed disabled:opacity-50`,
+                className,
+                { 'datePicker-icon-class': showIcon },
+              )}
+              ref={ref} // Ensure the ref is for HTMLInputElement
+              {...otherProps}
+            />
+          }
+          selected={value}
+          onChange={onChange}
+          dateFormat={dateFormat}
+        />
+      </div>
+    );
+  },
+);
+
+NewDatePicker.displayName = 'NewDatePicker'; // Required to set displayName for forwardRef component
 
 export default NewDatePicker;
